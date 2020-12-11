6192ecd235730fbc9b4e6dbdf0b2201e8a24bf68
Jacob Cassady <jtcass01@louisville.edu>
2020-12-11 09:35:33 -0500

    Reformatted tests to be unit tests. Adding dds connector test to futher verify

--- add_example.py
+++ add_example.py
@@ -1,5 +1,41 @@
-from ctypes import CDLL, c_float
+from ctypes import CDLL, c_float, RTLD_GLOBAL
 from os.path import join, isfile, abspath
+from unittest import TestCase, main
+
+
+class Ctypes_Test(TestCase):
+    def test_simple_add(self) -> None:
+        print("Performing Ctypes simple add test.")
+
+        ctypes_source_file_location = get_ctypes_source_file_location(c_types_source_file_name='adder.so')
+        assert isfile(ctypes_source_file_location), "Unable to locate file " + ctypes_source_file_location
+
+        #load the shared object file
+        adder = CDLL(ctypes_source_file_location)
+
+        #Find sum of integers
+        assert adder.add_int(4,5) == 9
+
+        #Find sum of floats
+        a = c_float(5.5)
+        b = c_float(4.1)
+
+        add_float = adder.add_float
+        add_float.restype = c_float
+        c_types_float_result = add_float(a, b)
+
+        assert round(c_types_float_result, 2) == round(9.6, 2)
+        print("Ctypes test was successful.")
+
+    def test_rti_dds_connector_load(self) -> None:
+        print("Performing rti dds connector load test.")
+
+        ctypes_source_file_location = get_ctypes_source_file_location(c_types_source_file_name='librtiddsconnector.so')
+        assert isfile(ctypes_source_file_location), "Unable to locate file " + ctypes_source_file_location
+
+        rti = CDLL(ctypes_source_file_location, RTLD_GLOBAL)
+
+        print("RTI DDS Connector load test was successful.")
 
 
 def get_ctypes_source_file_location(c_types_source_file_name: str) -> str:
@@ -14,24 +50,4 @@ def get_ctypes_source_file_location(c_types_source_file_name: str) -> str:
 
 
 if __name__ == "__main__":
-    ctypes_source_file_location = get_ctypes_source_file_location(c_types_source_file_name='adder.so')
-
-    assert isfile(ctypes_source_file_location), "Unable to locate file " + ctypes_source_file_location
-
-    #load the shared object file
-    adder = CDLL(ctypes_source_file_location)
-
-    #Find sum of integers
-    assert adder.add_int(4,5) == 9
-
-    #Find sum of floats
-    a = c_float(5.5)
-    b = c_float(4.1)
-
-    add_float = adder.add_float
-    add_float.restype = c_float
-    c_types_float_result = add_float(a, b)
-
-    assert round(c_types_float_result, 2) == round(9.6, 2)
-
-    print("Ctypes test was successful.")
+    main()
